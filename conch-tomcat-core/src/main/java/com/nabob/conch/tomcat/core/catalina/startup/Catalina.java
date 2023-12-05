90ef0fa366f3a28da4e6f9fc9cc9204e862e9e35
zjz6b6 <407091038@qq.com>
2023-12-05 13:23:26 +0800

    chore: add Tomcat and Catalina

---
+++ conch-tomcat-core/src/main/java/com/nabob/conch/tomcat/core/catalina/startup/Catalina.java
@@ -0,0 +1,12 @@
+package com.nabob.conch.tomcat.core.catalina.startup;
+
+/**
+ * Catalina（创始人）
+ * <p>
+ * 负责组建团队，也就是创建Server以及它的子组件
+ *
+ * @author Adam
+ * @since 2023/12/5
+ */
+public class Catalina {
+}
---
+++ conch-tomcat-core/src/main/java/com/nabob/conch/tomcat/core/catalina/startup/Tomcat.java
@@ -0,0 +1,10 @@
+package com.nabob.conch.tomcat.core.catalina.startup;
+
+/**
+ * Tomcat（公司）
+ *
+ * @author Adam
+ * @since 2023/12/5
+ */
+public class Tomcat {
+}
