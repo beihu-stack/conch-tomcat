10abdf7066c668e81ab8bd99e86e94135ac914a9
zjz6b6 <407091038@qq.com>
2023-12-05 20:16:39 +0800

    todo: add LifecycleBase support

--- conch-tomcat-core/src/main/java/com/nabob/conch/tomcat/core/catalina/lifecycle/LifecycleBase.java
+++ conch-tomcat-core/src/main/java/com/nabob/conch/tomcat/core/catalina/lifecycle/LifecycleBase.java
@@ -1,8 +1,13 @@
 package com.nabob.conch.tomcat.core.catalina.lifecycle;
 
 /**
+ * Base impl of Lifecycle
+ *
  * @author Adam
  * @since 2023/12/5
  */
 public abstract class LifecycleBase implements Lifecycle {
+
+
+
 }
