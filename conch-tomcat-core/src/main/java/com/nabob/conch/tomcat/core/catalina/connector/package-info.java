f4dc0d58d73c00ee96b06507d633c12ccb7cd27c
zjz6b6 <407091038@qq.com>
2023-12-05 20:19:02 +0800

    todo: add Connector

---
+++ conch-tomcat-core/src/main/java/com/nabob/conch/tomcat/core/catalina/connector/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 连接器
+ * <p>
+ * 用于实现与Coyote的连接
+ *
+ * @author Adam
+ * @since 2023/12/5
+ */
+package com.nabob.conch.tomcat.core.catalina.connector;
--- conch-tomcat-core/src/main/java/com/nabob/conch/tomcat/core/coyote/package-info.java
+++ conch-tomcat-core/src/main/java/com/nabob/conch/tomcat/core/coyote/package-info.java
@@ -1,5 +1,10 @@
 /**
- * Connector
+ * Coyote
+ * - 封装了底层的网络通信（Socket 请求及响应处理）
+ * - 使Catalina 容器（容器组件）与具体的请求协议及IO操作方式完全解耦
+ * - 将Socket 输入转换封装为 Request 对象，进一步封装后交由Catalina 容器进行处理，处理请求完成后, Catalina 通过Coyote 提供的Response 对象将结果写入输出流
+ * - 负责的是具体协议（应用层）和IO（传输层）相关内容
+ * - 一个连接器对应一个监听端口
  *
  * @author Adam
  * @since 2023/12/5
